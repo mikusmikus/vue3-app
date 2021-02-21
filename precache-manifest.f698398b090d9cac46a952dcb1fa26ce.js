46e30278eb151b9898c0a81229338892858f73b0
Mikus <mikusmeikulis@gmail.com>
2021-02-21 22:05:29 +0200

    deploy

---
+++ precache-manifest.f698398b090d9cac46a952dcb1fa26ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22668d6c0ca4350e222a",
+    "url": "/vue3-app/css/app.3a1c1a3d.css"
+  },
+  {
+    "revision": "1b9276b1dbee5b23c62e720e706ba8a0",
+    "url": "/vue3-app/index.html"
+  },
+  {
+    "revision": "22668d6c0ca4350e222a",
+    "url": "/vue3-app/js/app.a3f7c671.js"
+  },
+  {
+    "revision": "262b2e94f85d61d2237e",
+    "url": "/vue3-app/js/chunk-vendors.a9c77818.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/vue3-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-app/robots.txt"
+  }
+]);
